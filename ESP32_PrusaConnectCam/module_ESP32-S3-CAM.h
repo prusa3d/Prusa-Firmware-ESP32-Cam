220573519ac4958131a682af7cf475dc87e8774a
Miroslav Pivovarsky <miroslav.pivovarsky@compote.eu>
2024-07-12 22:00:01 +0200

    update

--- ESP32_PrusaConnectCam/module_ESP32-S3-CAM.h
+++ ESP32_PrusaConnectCam/module_ESP32-S3-CAM.h
@@ -56,46 +56,46 @@
 
 /* ------------------ MCU CFG  ------------------*/
 #define BOARD_NAME                  F("ESP32-S3-CAM") ///< Board name
-#define ENABLE_BROWN_OUT_DETECTION  false    ///< Enable brown out detection
-#define ENABLE_PSRAM                true     ///< Enable PSRAM   
+#define ENABLE_BROWN_OUT_DETECTION  false   ///< Enable brown out detection
+#define ENABLE_PSRAM                true    ///< Enable PSRAM   
 
 /* --------------- OTA UPDATE CFG  --------------*/
 #define OTA_UPDATE_FW_FILE          PSTR("esp32-s3-cam.bin") ///< OTA update firmware file name
-#define FW_STATUS_LED_PIN           2      ///< GPIO pin for status FW update LED
-#define FW_STATUS_LED_LEVEL_ON      HIGH   ///< GPIO pin level for status LED ON
+#define FW_STATUS_LED_PIN           2        ///< GPIO pin for status FW update LED
+#define FW_STATUS_LED_LEVEL_ON      HIGH     ///< GPIO pin level for status LED ON
 
 /* --------------- FLASH LED CFG  ---------------*/
-#define ENABLE_CAMERA_FLASH         true   ///< Enable camera flash function
-#define CAMERA_FLASH_DIGITAL_CTRL   true   ///< Enable camera flash digital control
-#define CAMERA_FLASH_PWM_CTRL       false  ///< Enable camera flash PWM control
-#define CAMERA_FLASH_NEOPIXEL       true   ///< Enable camera flash NeoPixel control
-#define FLASH_GPIO_NUM              47     ///< Flash control pin.
-#define FLASH_NEOPIXEL_LED_PIN      48     ///< External flash control pin. RGB LED NeoPixel
-#define FLASH_OFF_STATUS            LOW    ///< value for turn off flash
-#define FLASH_ON_STATUS             HIGH   ///< value for turn on flash
+#define ENABLE_CAMERA_FLASH         true     ///< Enable camera flash function
+#define CAMERA_FLASH_DIGITAL_CTRL   true     ///< Enable camera flash digital control
+#define CAMERA_FLASH_PWM_CTRL       false    ///< Enable camera flash PWM control
+#define CAMERA_FLASH_NEOPIXEL       true     ///< Enable camera flash NeoPixel control
+#define FLASH_GPIO_NUM              47       ///< Flash control pin.
+#define FLASH_NEOPIXEL_LED_PIN      48       ///< External flash control pin. RGB LED NeoPixel
+#define FLASH_OFF_STATUS            LOW      ///< value for turn off flash
+#define FLASH_ON_STATUS             HIGH     ///< value for turn on flash
 //#define FLASH_PWM_FREQ              2000    ///< frequency of pwm [240MHz / (100 prescale * pwm cycles)] = frequency
 //#define FLASH_PWM_CHANNEL           0       ///< channel 0
 //#define FLASH_PWM_RESOLUTION        8       ///< range 1-20bit. 8bit = 0-255 range
 
 /* --------------- SD CARD CFG  ---------------*/
-#define ENABLE_SD_CARD              true    ///< Enable SD card function
-#define SD_PIN_CLK                  39      ///< GPIO pin for SD card clock
-#define SD_PIN_CMD                  38      ///< GPIO pin for SD card command
-#define SD_PIN_DATA0                40      ///< GPIO pin for SD card data 0
+#define ENABLE_SD_CARD              true     ///< Enable SD card function
+#define SD_PIN_CLK                  39       ///< GPIO pin for SD card clock
+#define SD_PIN_CMD                  38       ///< GPIO pin for SD card command
+#define SD_PIN_DATA0                40       ///< GPIO pin for SD card data 0
 
 /* ---------- RESET CFG CONFIGURATION  ----------*/
-#define CFG_RESET_PIN               14      ///< GPIO 16 is for reset CFG to default
-#define CFG_RESET_LED_PIN           2       ///< GPIO for indication of reset CFG
-#define CFG_RESET_LED_LEVEL_ON      HIGH    ///< GPIO pin level for status LED ON
+#define CFG_RESET_PIN               14       ///< GPIO 16 is for reset CFG to default
+#define CFG_RESET_LED_PIN           2        ///< GPIO for indication of reset CFG
+#define CFG_RESET_LED_LEVEL_ON      HIGH     ///< GPIO pin level for status LED ON
 
 /* -------------- STATUS LED CFG ----------------*/
-#define STATUS_LED_ENABLE           true    ///< enable/disable status LED
-#define STATUS_LED_GPIO_NUM         2      ///< GPIO pin for status LED
+#define STATUS_LED_ENABLE           true     ///< enable/disable status LED
+#define STATUS_LED_GPIO_NUM         2        ///< GPIO pin for status LED
 #define STATUS_LED_OFF_PIN_LEVEL    HIGH     ///< GPIO pin level for status LED ON
 
 /* -------------- DHT SENSOR CFG ----------------*/
-#define DHT_SENSOR_ENABLE           true   ///< enable/disable DHT sensor
-#define DHT_SENSOR_PIN              20     ///< GPIO pin for DHT sensor
+#define DHT_SENSOR_ENABLE           true     ///< enable/disable DHT sensor
+#define DHT_SENSOR_PIN              20       ///< GPIO pin for DHT sensor
 
 #endif
 /* EOF */
