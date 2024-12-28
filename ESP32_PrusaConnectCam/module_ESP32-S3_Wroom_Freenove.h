e93f7ee0e4e4750532540892684fc559e67bea90
Miroslav Pivovarsky <miroslav.pivovarsky@compote.eu>
2024-12-28 12:55:25 +0100

    update pinout for ESP32-S3 wroom Freenove

--- ESP32_PrusaConnectCam/module_ESP32-S3_Wroom_Freenove.h
+++ ESP32_PrusaConnectCam/module_ESP32-S3_Wroom_Freenove.h
@@ -68,10 +68,10 @@
 
 /* --------------- FLASH LED CFG  ---------------*/
 #define ENABLE_CAMERA_FLASH         true    ///< Enable camera flash function
-#define CAMERA_FLASH_DIGITAL_CTRL   false   ///< Enable camera flash digital control
+#define CAMERA_FLASH_DIGITAL_CTRL   true   ///< Enable camera flash digital control
 #define CAMERA_FLASH_PWM_CTRL       false   ///< Enable camera flash PWM control
 #define CAMERA_FLASH_NEOPIXEL       true    ///< Enable camera flash NeoPixel control
-#define FLASH_GPIO_NUM              -1      ///< Flash control pin
+#define FLASH_GPIO_NUM              14      ///< Flash control pin
 #define FLASH_NEOPIXEL_LED_PIN      48      ///< External flash control pin. RGB LED NeoPixel
 #define FLASH_OFF_STATUS            0       ///< PWM intensity LED for OFF. 0-2^FLASH_PWM_RESOLUTION = 0-255
 #define FLASH_ON_STATUS             205     ///< PWM intensity LED for ON. limitation to 80%. 2^FLASH_PWM_RESOLUTION * 0.8% = 204
